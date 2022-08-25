9791ee24a84a8e378455a46290f7dbda98038707
Aleksei Vetrov <vvvvvv@google.com>
2022-08-25 17:08:18 +0100

    ELF reader: implement basic symbol filtering in ELF processing
    * Add function-wrapper `AllowIf` to easily filter vector of symbols using predicate
    * Leave only defined functions and variables in symbols
    
    PiperOrigin-RevId: 470003943
    Change-Id: I29d948a3e1f09532c554544cb15f09170ff2c0ba

--- elf_reader.cc
+++ elf_reader.cc
@@ -19,9 +19,13 @@
 
 #include "elf_reader.h"
 
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <string>
+#include <vector>
 
 #include "elf_loader.h"
 #include "stg.h"
@@ -31,10 +35,59 @@ namespace elf {
 
 namespace {
 
+using SymbolTable = std::vector<SymbolTableEntry>;
+
+bool IsPublicFunctionOrVariable(const SymbolTableEntry& symbol) {
+  const auto symbol_type = symbol.symbol_type;
+  // Reject symbols that are not functions or variables.
+  if (symbol_type != SymbolTableEntry::SymbolType::FUNCTION &&
+      symbol_type != SymbolTableEntry::SymbolType::OBJECT &&
+      symbol_type != SymbolTableEntry::SymbolType::TLS)
+    return false;
+
+  // Function or variable of ValueType::ABSOLUTE is not expected in any binary,
+  // but GNU `ld` adds object of such type for every version name defined in
+  // file. Such symbol should be rejected, because in fact it is not variable.
+  if (symbol.value_type == SymbolTableEntry::ValueType::ABSOLUTE) {
+    Check(symbol_type == SymbolTableEntry::SymbolType::OBJECT)
+        << "Unexpected function or variable with ABSOLUTE value type";
+    return false;
+  }
+
+  // Undefined symbol is dependency of the binary but is not part of ABI
+  // provided by binary and should be rejected.
+  if (symbol.value_type == SymbolTableEntry::ValueType::UNDEFINED)
+    return false;
+
+  // Local symbol is not visible outside the binary, so it is not public
+  // and should be rejected.
+  if (symbol.binding == SymbolTableEntry::Binding::LOCAL)
+    return false;
+
+  // "Hidden" and "internal" visibility values mean that symbol is not public
+  // and should be rejected.
+  if (symbol.visibility == SymbolTableEntry::Visibility::HIDDEN ||
+      symbol.visibility == SymbolTableEntry::Visibility::INTERNAL)
+    return false;
+
+  return true;
+}
+
 Id Read(Graph& graph, elf::ElfLoader&& elf, bool verbose) {
-  const auto symbols = elf.GetElfSymbols();
+  const auto all_symbols = elf.GetElfSymbols();
   if (verbose)
-    std::cout << "Parsed " << symbols.size() << " symbols\n";
+    std::cout << "Parsed " << all_symbols.size() << " symbols\n";
+
+  std::vector<SymbolTableEntry> public_functions_and_variables;
+  public_functions_and_variables.reserve(all_symbols.size());
+  std::copy_if(all_symbols.begin(), all_symbols.end(),
+               std::back_inserter(public_functions_and_variables),
+               IsPublicFunctionOrVariable);
+  public_functions_and_variables.shrink_to_fit();
+
+  if (verbose)
+    std::cout << "File has " << public_functions_and_variables.size()
+              << " public functions and variables\n";
 
   std::map<SymbolKey, Id> symbols_map;
   return graph.Add(Make<Symbols>(symbols_map));
