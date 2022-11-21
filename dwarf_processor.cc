cdf916f243f20afd91f17ceafc6139fb60a23586
Aleksei Vetrov <vvvvvv@google.com>
2022-11-21 14:27:33 +0000

    DWARF reading: add switch to process DWARF entries based on tag
    Introduce a pattern, where an entry by default is passed to the big
    switch with all possible DWARF entry types (tags) and is then being
    dispatched to a specific processor method.
    
    PiperOrigin-RevId: 489964727
    Change-Id: Ic4b3012cda442fc3f7567c1a4f10ef320984f6a8

--- dwarf_processor.cc
+++ dwarf_processor.cc
@@ -1,8 +1,11 @@
 #include "dwarf_processor.h"
 
+#include <dwarf.h>
+
 #include <vector>
 
 #include "dwarf.h"
+#include "error.h"
 
 namespace stg {
 namespace dwarf {
@@ -14,7 +17,20 @@ class Processor {
 
   void Process(Entry& entry) {
     ++result_.processed_entries;
-    ProcessAllChildren(entry);
+    auto tag = entry.GetTag();
+    switch (tag) {
+      case DW_TAG_compile_unit:
+        ProcessCompileUnit(entry);
+        break;
+      case DW_TAG_base_type:
+        ProcessBaseType(entry);
+        break;
+
+      default:
+        // TODO: die on unexpected tag, when this switch contains
+        // all expected tags
+        break;
+    }
   }
 
  private:
@@ -22,6 +38,20 @@ class Processor {
     for (auto& child : entry.GetChildren()) {
       Process(child);
     }
+  }
+
+  void CheckNoChildren(Entry& entry) {
+    if (!entry.GetChildren().empty()) {
+      Die() << "Entry expected to have no children";
+    }
+  }
+
+  void ProcessCompileUnit(Entry& entry) {
+    ProcessAllChildren(entry);
+  }
+
+  void ProcessBaseType(Entry& entry) {
+    CheckNoChildren(entry);
   }
 
   Types& result_;
