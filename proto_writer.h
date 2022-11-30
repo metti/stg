61fc158196fc6ad8950ee3b2f51da14ec9b239fa
Sid Nayyar <sidnayyar@google.com>
2022-11-30 13:36:00 +0000

    proto: Add declaration for protobuf writer
    This interface will be used for serialising the STG internal graph to
    the protobuf format.
    
    PiperOrigin-RevId: 491889937
    Change-Id: I628cfd1c1a43dcf32d2ff88003ae4f41ee9bcf19

---
+++ proto_writer.h
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// -*- mode: C++ -*-
+//
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions (the
+// "License"); you may not use this file except in compliance with the
+// License.  You may obtain a copy of the License at
+//
+//     https://llvm.org/LICENSE.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Siddharth Nayyar
+
+#ifndef STG_PROTO_WRITER_H_
+#define STG_PROTO_WRITER_H_
+
+#include "stg.h"
+
+namespace stg {
+namespace proto {
+
+class Writer {
+ public:
+  Writer(const stg::Graph& graph) : graph_(graph) {}
+
+ private:
+  [[maybe_unused]] const stg::Graph& graph_;
+};
+
+}  // namespace proto
+}  // namespace stg
+
+#endif  // STG_PROTO_WRITER_H_
