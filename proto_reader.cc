796061cef25272ade928e095cc19333815510ebf
Sid Nayyar <sidnayyar@google.com>
2023-01-17 11:25:53 +0000

    proto: Check reader input file stream can read file with given path
    Exit the program with a meaningful error message when the proto reader
    can not read from the given file.
    
    PiperOrigin-RevId: 502538260
    Change-Id: I685e3af5affdf1e8ac078d94a76c52d6a2582644

--- proto_reader.cc
+++ proto_reader.cc
@@ -405,6 +405,8 @@ Type Transformer::Transform(const Type& x) {
 
 Id Read(Graph& graph, const std::string& path) {
   std::ifstream ifs(path);
+  Check(ifs.good()) << "error opening file '" << path
+                    << "' for reading: " << ErrnoToString(errno);
   google::protobuf::io::IstreamInputStream is(&ifs);
   proto::STG stg;
   google::protobuf::TextFormat::Parse(&is, &stg);
